1e5a0dc301a109b70b2c3ed13115234c66123d37
Богдан Куликов <realggaster@gmail.com>
2020-01-12 18:28:43 +0300

    All tasks are completed

--- asciimg.go
+++ asciimg.go
@@ -5,13 +5,24 @@ import (
 	"fmt"
 	"image"
 	"image/color"
-	_ "image/jpeg"
+
 	// Side-effect import.
 	// Сайд-эффект — добавление декодера PNG в пакет image.
+	_ "image/jpeg"
 	_ "image/png"
 	"os"
-	// Внешняя зависимость.
+
+	"github.com/buger/goterm"
+	colorTerm "github.com/fatih/color"
 	"golang.org/x/image/draw"
+)
+
+var (
+	fileRead = flag.String("o", "", "Output path")
+	noScale  = flag.Bool("noscale", false, "Scales picture with <w> and <h> as separate flags")
+	w        = flag.Int("w", goterm.Width(), "width of ascii picture")
+	h        = flag.Int("h", goterm.Height(), "height of ascii picture")
+	isColor  = flag.Bool("c", false, "make ascii picture colorful")
 )
 
 func scale(img image.Image, w int, h int) image.Image {
@@ -25,23 +36,25 @@ func decodeImageFile(imgName string) (image.Image, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	img, _, err := image.Decode(imgFile)
-
 	return img, err
 }
 
 func processPixel(c color.Color) rune {
+	symbols := []rune("@80GCLft1i;:,. ")
 	gc := color.GrayModel.Convert(c)
 	r, _, _, _ := gc.RGBA()
 	r = r >> 8
-
-	// symbols := []rune("@80GCLft1i;:,. ")
-	return '0'
+	if r == 255 {
+		//Костыль, но не могу придумать, как пофиксить.
+		r = 250
+	}
+	return symbols[int(r/(255/uint32(len(symbols))))%(len(symbols))]
 }
 
-func convertToAscii(img image.Image) [][]rune {
+func convertToASCII(img image.Image) [][]rune {
 	textImg := make([][]rune, img.Bounds().Dy())
+	// i := 0; i < i.Bound().Dy; i++
 	for i := range textImg {
 		textImg[i] = make([]rune, img.Bounds().Dx())
 	}
@@ -57,23 +70,57 @@ func convertToAscii(img image.Image) [][]rune {
 func main() {
 	flag.Parse()
 
+	var (
+		Cyan   = colorTerm.New(colorTerm.FgCyan)
+		Red    = colorTerm.New(colorTerm.FgRed)
+		Yellow = colorTerm.New(colorTerm.FgYellow)
+	)
 	if flag.NArg() == 0 {
 		fmt.Println("Usage: asciimg <image.jpg>")
 		os.Exit(0)
 	}
-	imgName := flag.Arg(0)
 
-	img, err := decodeImageFile(imgName)
+	img := flag.Arg(0)
+	image, err := decodeImageFile(img)
 	if err != nil {
 		fmt.Println("Error:", err.Error())
 		os.Exit(1)
 	}
-
-	textImg := convertToAscii(img)
-	for i := range textImg {
-		for j := range textImg[i] {
-			fmt.Printf("%c", textImg[i][j])
+	if *fileRead == "" && !*noScale {
+		image = scale(image, *w, *h)
+	}
+	textImg := convertToASCII(image)
+	if *fileRead != "" {
+		f, err := os.Create(*fileRead)
+		defer f.Close()
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(0)
 		}
-		fmt.Println()
+		for i := range textImg {
+			for j := range textImg[i] {
+
+				fmt.Fprintf(f, "%c", textImg[i][j])
+			}
+		}
+		fmt.Fprintln(f)
+
+	} else {
+		for i := range textImg {
+			for j := range textImg[i] {
+				if *isColor {
+					if textImg[i][j] == '@' || textImg[i][j] == '8' || textImg[i][j] == '0' || textImg[i][j] == 'G' {
+						Cyan.Printf("%c", textImg[i][j])
+					} else if textImg[i][j] == 'C' || textImg[i][j] == 'L' || textImg[i][j] == 'f' || textImg[i][j] == 't' {
+						Red.Printf("%c", textImg[i][j])
+					} else {
+						Yellow.Printf("%c", textImg[i][j])
+					}
+				} else {
+					fmt.Printf("%c", textImg[i][j])
+				}
+			}
+			fmt.Println()
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
 module asciimg
 
-go 1.12
+go 1.13
 
-require golang.org/x/image v0.0.0-20191214001246-9130b4cfad52
+require (
+	github.com/buger/goterm v0.0.0-20181115115552-c206103e1f37
+	github.com/fatih/color v1.9.0
+	golang.org/x/image v0.0.0-20191214001246-9130b4cfad52
+)
